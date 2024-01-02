674b6c40277f79b20fe0b42830f8241a0f513045
MichalRzeszot <134526382+MichalRzeszot@users.noreply.github.com>
2024-01-02 17:48:59 +0000

    Deploying to gh-pages from @ MichalRzeszot/goit-group-project-bookshelf@f9af977945dbb22898300d9adf690425eeba2acf 🚀

--- index.fc349f44.js
+++ index.fc349f44.js
@@ -1,4 +1,4 @@
-const o=document.querySelector(".bl-header"),l=document.querySelector(".bl-container");async function t(o="top-books"){let t,e;"All Categories"===o&&(o="top-books"),"top-books"!=o&&(o=`category?category=${o}`);let a=await fetch("https://books-backend.p.goit.global/books/"+o),i=await a.json();"top-books"===o?(t="",i.forEach(o=>{t+=`<p class="bl-book-category">${o.list_name}</p><ul class="bl-books-list">`,o.books.forEach(o=>{t+=`<li class="bl-book-card bl-all-books" id="${o._id}">
+const o=document.querySelector(".bl-header"),l=document.querySelector(".bl-container");async function t(o="top-books"){let t,e,a=o;"All Categories"===a&&(a="top-books"),"top-books"!=a&&(a=`category?category=${a}`);let i=await fetch("https://books-backend.p.goit.global/books/"+a),b=await i.json();"top-books"===a?(t="",b.forEach(o=>{t+=`<p class="bl-book-category">${o.list_name}</p><ul class="bl-books-list">`,o.books.forEach(o=>{t+=`<li class="bl-book-card bl-all-books" id="${o._id}">
       <div class="bl-book-image">
       <img src="${o.book_image}" alt="${o.title}"/>
       <div class="bl-book-card-overlay">
@@ -7,7 +7,7 @@ const o=document.querySelector(".bl-header"),l=document.querySelector(".bl-conta
       </div>
       <p class="bl-book-title">${o.title}</p>
       <p class="bl-book-author">${o.author}</p>
-      </li>`}),t+=`</ul><button id="${o.list_name}"class="bl-see-more-btn">SEE MORE</button>`}),l.innerHTML=t,document.querySelectorAll(".bl-see-more-btn").forEach(o=>{o.addEventListener("click",s)})):(e='<ul class="bl-books-list">',i.forEach(o=>{e+=`<li class="bl-book-card" id="${o._id}">
+      </li>`}),t+=`</ul><button id="${o.list_name}"class="bl-see-more-btn">SEE MORE</button>`}),l.innerHTML=t,document.querySelectorAll(".bl-see-more-btn").forEach(o=>{o.addEventListener("click",s)})):(e='<ul class="bl-books-list">',b.forEach(o=>{e+=`<li class="bl-book-card" id="${o._id}">
       <div class="bl-book-image">
       <img src="${o.book_image}" alt="${o.title}"/>
       <div class="bl-book-card-overlay">
@@ -17,4 +17,4 @@ const o=document.querySelector(".bl-header"),l=document.querySelector(".bl-conta
       <p class="bl-book-title">${o.title}</p>
       <p class="bl-book-author">${o.author}</p>
       </li>`}),e+="</ul>",l.innerHTML=e)}function s(){e(this.id),t(this.id),scroll(0,0)}function e(l){"All Categories"===l&&(l="Best Sellers Books");let t=l.split(" "),s=t.pop(),e=t.join(" ");o.innerHTML=`${e} <span class="bl-span">${s} </span>`}o.innerHTML='Best Sellers <span class="bl-span">Books</span>',t(),placeForList.addEventListener("click",o=>{e(o.target.textContent),t(o.target.textContent)});
-//# sourceMappingURL=index.8d82f423.js.map
+//# sourceMappingURL=index.fc349f44.js.map
