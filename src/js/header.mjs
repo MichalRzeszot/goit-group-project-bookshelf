5b7e80060c6c8f00aa31a9945c81889514dbe791
WojtekKirsz <w.kirsz@gmail.com>
2024-01-03 22:48:37 +0100

    firebase modal updates in header

--- src/js/header.mjs
+++ src/js/header.mjs
@@ -38,9 +38,9 @@
   const signUpClick = () => {
     signUpBtn.classList.add('hidden');
     signUpForm.classList.remove('hidden');
-    header.classList.add('hidden');
+    // header.classList.add('hidden');
     mobileMenu.classList.remove('bg-books');
-    mobileMenu.classList.add('bg-form');
+    // mobileMenu.classList.add('bg-form');
   };
   signUpBtn.addEventListener('click', signUpClick);
 
