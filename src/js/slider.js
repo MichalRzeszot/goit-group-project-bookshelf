41adcd1b477b79960c3662626d9f4ab348f54b1c
Martyna00038 <martyna00038@gmail.com>
2024-01-03 13:32:17 +0100

    slider - html + css

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,12 @@
-const charities = ['1', '2', '3', '4', '5'];
+const charities = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  // '07', '08', '09'
+];
 
 const charitiesList = document.querySelector('#charities');
 const list = [];
@@ -6,7 +14,7 @@ const list = [];
 for (const element of charities) {
   const charity = document.createElement('li');
   charity.textContent = element;
-  charity.classList.add('item');
+  charity.classList.add('charity');
   list.push(charity);
 }
 
