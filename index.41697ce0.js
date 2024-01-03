9db317c1162e6f907832f84e50064a69c0edd64b
MichalRzeszot <134526382+MichalRzeszot@users.noreply.github.com>
2024-01-03 18:46:14 +0000

    Deploying to gh-pages from @ MichalRzeszot/goit-group-project-bookshelf@2cb8e75d16024a63c9d31da7c2b626fa7abc2a36 🚀

---
+++ index.41697ce0.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function(){let o=JSON.parse(localStorage.getItem("shoppingList"))||[],t=document.getElementById("shopping-list");0===o.length?t.innerHTML='<p class="sl-paragraph">This page is empty, add some books and proceed to order.</p>':o.forEach(o=>{let e=function(o){let t=document.createElement("div");return t.classList.add("book-card"),t.innerHTML=`
+            <img src="${o.book_image}" alt="${o.title} Cover">
+            <div class="book-title">${o.title}</div>
+            <div class="book-category">${o.list_name}</div>
+            <div class="book-description">${o.description}</div>
+            <div class="book-author">${o.author}</div>
+            // dopracowa\u{107} - poni\u{17C}ej ma by\u{107} lista link\xf3w
+            <div class="purchase-links">
+                ${o.buy_links}
+            </div> 
+            // dopracowa\u{107} - obrazek kosza powinien tutaj by\u{107}
+            <button onclick="removeFromShoppingList('${o._id}')">TRASH</button>
+        `,t}(o);t.appendChild(e)}),document.removeFromShoppingList=function(t){let e=o.filter(o=>o._id!==t);localStorage.setItem("shoppingList",JSON.stringify(e)),location.reload()}});
+//# sourceMappingURL=index.41697ce0.js.map
