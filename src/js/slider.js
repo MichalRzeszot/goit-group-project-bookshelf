97c35747d067568b6b71b2879c73e59e7a88e853
Martyna00038 <martyna00038@gmail.com>
2024-01-04 23:37:14 +0100

    Slider - js bez paginacji

---
+++ src/js/slider.js
@@ -0,0 +1,87 @@
+const charities = [
+  `<p>01</p><img class="slider-img-01" src="${require('../images/slider-img/01.png')}" />`,
+  `<p>02</p><img class="slider-img-02" src="${require('../images/slider-img/02.png')}" />`,
+  `<p>03</p><img class="slider-img-03" src="${require('../images/slider-img/03.png')}" />`,
+  `<p>04</p><img class="slider-img-04" src="${require('../images/slider-img/04.png')}" />`,
+  `<p>05</p><img class="slider-img-05" src="${require('../images/slider-img/05.png')}" />`,
+  `<p>06</p><img class="slider-img-06" src="${require('../images/slider-img/06.png')}" />`,
+  // `<p>07</p><img class="slider-img-07" src="${require('../images/slider-img/07.png')}" />`,
+  // `<p>08</p><img class="slider-img-08" src="${require('../images/slider-img/08.png')}" />`,
+  // `<p>09</p><img class="slider-img-09" src="${require('../images/slider-img/09.png')}" />`,
+];
+
+const array = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: null,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: null,
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: null,
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: null,
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: null,
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: null,
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: null,
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: null,
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: null,
+  },
+];
+
+const charitiesList = document.querySelector('#charities');
+const list = [];
+
+charities.forEach((element, index) => {
+  const li = document.createElement('li');
+  li.innerHTML = `<a class="charity-container" href="${array[index].url}">${element}</a>`;
+  li.classList.add('charity');
+  list.push(li);
+});
+
+// for (const element of charities) {
+//   const charity = document.createElement('li');
+//   charity.textContent = element;
+//   charity.classList.add('charity');
+//   list.push(charity);
+// }
+
+charitiesList.append(...list);
+
+// ---------------------------------------
+
+const button = document.querySelector('.slider-button');
+
+// element.addEventListener(
+//   'click',
+//   // handler, options
+// );
