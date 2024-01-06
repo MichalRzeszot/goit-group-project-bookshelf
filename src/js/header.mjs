acb05a0cae348287e1a6367328789c7abb8c3ddd
inezkaj <ienzkajed@icloud.com>
2024-01-06 17:00:09 +0100

    header-logowanie

--- src/js/header.mjs
+++ src/js/header.mjs
@@ -1,64 +1,86 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const toggleMenuBtn = document.querySelector('.js-toggle-menu');
-  const header = document.querySelector('.header-frame');
-  const openShoppingMenu = document.querySelector('.shopping-item');
-  const signUpBtn = document.querySelector('.sign-up-btn');
-  const signUpForm = document.querySelector('.sign-up-form');
-  const signUpFormBtn = document.querySelector('.sign-up-form-btn');
-  const signUpContent = document.querySelector('.sign-up-content');
-  const userMenuContent = document.querySelector('.user-menu');
+import { getAuth } from 'firebase/auth';
 
-  const toggleMenu = () => {
-    console.log('toggleMenu');
-    // czy menu jest otwarte
-    const isMenuOpen = toggleMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    toggleMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+const mobileMenu = document.querySelector('.js-menu-container');
+const toggleMenuBtn = document.querySelector('.js-toggle-menu');
+const header = document.querySelector('.header-frame');
+const openShoppingMenu = document.querySelector('.shopping-item');
+const signUpBtn = document.querySelector('.sign-up-btn');
+const signUpForm = document.querySelector('.sign-up-form');
+const signUpFormBtn = document.querySelector('#sign-up-form .modal__send');
+const signUpContent = document.querySelector('.sign-up-content');
+const userMenuContent = document.querySelector('.user-menu');
+const logOutBtn = document.querySelector('.log-out-btn');
 
-    mobileMenu.classList.remove('bg-form');
+const auth = getAuth();
+// auth.currentUser.displayName
 
-    if (isMenuOpen) {
-      console.log('opened menu');
-      header.classList.remove('menu-opened');
-      toggleMenuBtn.classList.add('hamburger-button');
-      toggleMenuBtn.classList.remove('close-btn');
-    } else {
-      console.log('closed menu');
-      header.classList.add('menu-opened');
-      mobileMenu.classList.remove('bg-white');
-      mobileMenu.classList.add('bg-books');
-      toggleMenuBtn.classList.remove('hamburger-button');
-      toggleMenuBtn.classList.add('close-btn');
-    }
-  };
+function closeHamburgerMenu() {
+  header.classList.remove('menu-opened');
+  toggleMenuBtn.classList.add('hamburger-button');
+  toggleMenuBtn.classList.remove('close-btn');
+}
 
-  toggleMenuBtn.addEventListener('click', toggleMenu);
+function openHamburgerMenu() {
+  header.classList.add('menu-opened');
+  mobileMenu.classList.remove('bg-white');
+  mobileMenu.classList.add('bg-books');
+  toggleMenuBtn.classList.remove('hamburger-button');
+  toggleMenuBtn.classList.add('close-btn');
 
-  const signUpClick = () => {
-    signUpBtn.classList.add('hidden');
-    signUpForm.classList.remove('hidden');
-    // header.classList.add('hidden');
-    mobileMenu.classList.remove('bg-books');
-    // mobileMenu.classList.add('bg-form');
-  };
-  signUpBtn.addEventListener('click', signUpClick);
+  if (auth.currentUser) {
+    userMenu();
+  }
+}
 
-  const userMenu = () => {
-    header.classList.remove('hidden');
-    signUpForm.classList.add('hidden');
-    mobileMenu.classList.add('bg-books');
-    mobileMenu.classList.remove('bg-form');
-    signUpContent.classList.add('hidden');
-    userMenuContent.classList.remove('hidden');
-  };
-  signUpFormBtn.addEventListener('click', userMenu);
+const toggleMenu = () => {
+  console.log('toggleMenu');
+  // czy menu jest otwarte
+  const isMenuOpen = toggleMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  toggleMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+  mobileMenu.classList.toggle('is-open');
 
-  const showShoppingList = () => {
-    //dodanie shopping-list
-    toggleMenu();
-    console.log('showShoppingList...');
-  };
+  mobileMenu.classList.remove('bg-form');
 
-  openShoppingMenu.addEventListener('click', showShoppingList);
-})();
+  if (isMenuOpen) {
+    console.log('opened menu');
+    closeHamburgerMenu();
+  } else {
+    console.log('closed menu');
+    openHamburgerMenu();
+  }
+};
+
+toggleMenuBtn.addEventListener('click', toggleMenu);
+
+const signUpClick = () => {
+  signUpBtn.classList.add('hidden');
+  signUpForm.classList.remove('hidden');
+  // header.classList.add('hidden');
+  mobileMenu.classList.remove('bg-books');
+  // mobileMenu.classList.add('bg-form');
+  mobileMenu.classList.remove('is-open');
+
+  closeHamburgerMenu();
+};
+signUpBtn.addEventListener('click', signUpClick);
+
+const userMenu = () => {
+  header.classList.remove('hidden');
+  signUpForm.classList.add('hidden');
+  mobileMenu.classList.add('bg-books');
+  mobileMenu.classList.remove('bg-form');
+  signUpContent.classList.add('hidden');
+  userMenuContent.classList.remove('hidden');
+  document.querySelector('.user-menu .button-user .user-text').textContent =
+    auth.currentUser.displayName;
+  // console.log(auth.currentUser.displayName);
+};
+// signUpFormBtn.addEventListener('click', userMenu);
+
+const showShoppingList = () => {
+  //dodanie shopping-list
+  toggleMenu();
+  console.log('showShoppingList...');
+};
+
+openShoppingMenu.addEventListener('click', showShoppingList);
